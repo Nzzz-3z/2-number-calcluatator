cf8f0546a6f9f08530400fbff2710ec9422357ed
Nadeem Ansari <nadeem.nadeem@gmail.com>
2024-03-25 14:38:41 +0530

    Add project files.

---
+++ 2 number calcluatator/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace _2_number_calcluatator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.optionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.davloperToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.theameAndColourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pickColourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ActionLB = new System.Windows.Forms.Label();
+            this.EqualLB = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionToolStripMenuItem,
+            this.theameAndColourToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(438, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // optionToolStripMenuItem
+            // 
+            this.optionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.davloperToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.optionToolStripMenuItem.Name = "optionToolStripMenuItem";
+            this.optionToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.optionToolStripMenuItem.Text = "Option";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("exitToolStripMenuItem.Image")));
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // davloperToolStripMenuItem
+            // 
+            this.davloperToolStripMenuItem.Name = "davloperToolStripMenuItem";
+            this.davloperToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.davloperToolStripMenuItem.Text = "Davloper";
+            // 
+            // theameAndColourToolStripMenuItem
+            // 
+            this.theameAndColourToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pickColourToolStripMenuItem});
+            this.theameAndColourToolStripMenuItem.Name = "theameAndColourToolStripMenuItem";
+            this.theameAndColourToolStripMenuItem.Size = new System.Drawing.Size(123, 20);
+            this.theameAndColourToolStripMenuItem.Text = "Theame and Colour";
+            // 
+            // pickColourToolStripMenuItem
+            // 
+            this.pickColourToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("pickColourToolStripMenuItem.Image")));
+            this.pickColourToolStripMenuItem.Name = "pickColourToolStripMenuItem";
+            this.pickColourToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pickColourToolStripMenuItem.Text = "Pick Colour";
+            this.pickColourToolStripMenuItem.Click += new System.EventHandler(this.pickColourToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(223, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 110);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(223, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 182);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(223, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(95, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "None";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.label3.Location = new System.Drawing.Point(103, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 39);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "=";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(262, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Enter number 1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(261, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Enter number 2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(264, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Result";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.Location = new System.Drawing.Point(24, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 53);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button2.Location = new System.Drawing.Point(105, 255);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 53);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button3.Location = new System.Drawing.Point(186, 255);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 53);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "X";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button4.Location = new System.Drawing.Point(267, 255);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 53);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "÷";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button5.Location = new System.Drawing.Point(348, 255);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 53);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "%";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.EqualLB);
+            this.panel1.Controls.Add(this.ActionLB);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(438, 331);
+            this.panel1.TabIndex = 15;
+            // 
+            // ActionLB
+            // 
+            this.ActionLB.AutoSize = true;
+            this.ActionLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.ActionLB.Location = new System.Drawing.Point(90, 67);
+            this.ActionLB.Name = "ActionLB";
+            this.ActionLB.Size = new System.Drawing.Size(59, 25);
+            this.ActionLB.TabIndex = 15;
+            this.ActionLB.Text = "None";
+            // 
+            // EqualLB
+            // 
+            this.EqualLB.AutoSize = true;
+            this.EqualLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.EqualLB.Location = new System.Drawing.Point(105, 144);
+            this.EqualLB.Name = "EqualLB";
+            this.EqualLB.Size = new System.Drawing.Size(24, 25);
+            this.EqualLB.TabIndex = 16;
+            this.EqualLB.Text = "=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 355);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "2 number Calclutator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem optionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem davloperToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem theameAndColourToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pickColourToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label EqualLB;
+        private System.Windows.Forms.Label ActionLB;
+    }
+}
+
---
+++ 2 number calcluatator/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_number_calcluatator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+            /* 
+             * double.Parse(textBox1.Text);
+            double.Parse(textBox2.Text);
+            this code for dubble value convent to textbox velue
+            */
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1, num2;
+            num1 = double.Parse(textBox1.Text);
+            num2 = double.Parse(textBox2.Text);
+            double result = num1 + num2;
+            textBox3.Text = ("" + result);
+            ActionLB.Text = ("+");
+            MessageBox.Show("Result is " + result, "Result" , MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double num1, num2;
+            num1 = double.Parse(textBox1.Text);
+            num2 = double.Parse(textBox2.Text);
+            double result = num1 - num2;
+            textBox3.Text = ("" + result);
+            ActionLB.Text = ("-");
+            MessageBox.Show("Result is " + result, "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double num1, num2;
+            num1 = double.Parse(textBox1.Text);
+            num2 = double.Parse(textBox2.Text);
+            double result = num1 * num2;
+            textBox3.Text = ("" + result);
+            ActionLB.Text = ("X");
+            MessageBox.Show("Result is " + result, "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double num1, num2;
+            num1 = double.Parse(textBox1.Text);
+            num2 = double.Parse(textBox2.Text);
+            double result = num1 / num2;
+            textBox3.Text = ("" + result);
+            ActionLB.Text = ("÷");
+            MessageBox.Show("Result is " + result, "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void pickColourToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.AllowFullOpen = true;
+            colorDialog.Color = panel1.BackColor;
+            colorDialog.FullOpen = true;
+            colorDialog.AnyColor = true;
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                panel1.BackColor = colorDialog.Color;
+            }
+            else
+            {
+                MessageBox.Show("You can not Slect Colour", "Colour naot Slect", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
